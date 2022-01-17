52821ef24e05f8faeb53f8546526c2dd6c63ea7d
Andres Ferrer <andresdavidferrer@hotmail.com>
2022-01-17 15:01:25 -0300

    first mod

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -6,8 +6,11 @@ from AppCoder.views import Curso
 
 urlpatterns = [
     path('', views.inicio, name="Inicio"), #esta era nuestra primer view
-    path('cursos', views.cursos, name="Cursos"),
-    path('profesores', views.profesores, name="Profesores"),
-    path('estudiantes', views.estudiantes, name="Estudiantes"),
-    path('entregables', views.entregables, name="Entregables"),
+    path('cursos/', views.cursos, name="Cursos"),
+    path('profesores/', views.profesores, name="Profesores"),
+    path('estudiantes/', views.estudiantes, name="Estudiantes"),
+    path('entregables/', views.entregables, name="Entregables"),
+    path('cursoFormulario/', views.cursoformulario, name="CursoFormulario"),
+    path('busquedaCamada/', views.busquedaCamada, name="BusquedaCamada"),
+    path('buscar/', views.buscar, name="Buscar"),
 ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse, request
+from AppCoder.forms import CursoFormulario
 from AppCoder.models import Curso
 
 # Create your views here.
@@ -12,6 +12,46 @@ def crea_curso(req, nombre, camada):
     curso.save()
 
     return HttpResponse(f'Se creo el curso de {curso.nombre} con el numero de camada {curso.camada}')
+
+def cursoformulario(req):
+
+    if(req.method == 'POST'):
+
+        mi_formulario = CursoFormulario(req.POST)
+
+        if (mi_formulario.is_valid()):
+
+            data = mi_formulario.cleaned_data
+        
+            curso = Curso (nombre=data['nombre'], camada=data['camada'])
+            curso.save()
+
+            return render(req, 'AppCoder/inicio.html')
+
+    else:
+
+        mi_formulario = CursoFormulario()
+
+    return render(req, 'AppCoder/cursoFormulario.html', {'form': mi_formulario})
+
+def busquedaCamada(req):
+
+    return render(req, 'AppCoder/busquedaCamada.html')
+
+def buscar(req):
+
+    if(req.method == "GET"):
+
+        camada = req.GET["camada"]
+        cursos = Curso.objects.filter(camada=camada)
+
+        return render(req, "AppCoder/cursos.html", {"cursos": cursos, "camada": camada})
+
+    else:
+
+        return HttpResponse(f'No enviaste datos')
+
+    # return HttpResponse(f'Estamos buscando los cursos de la camada numero: {req.GET["camada"]}')
 
 def inicio(req):
 
